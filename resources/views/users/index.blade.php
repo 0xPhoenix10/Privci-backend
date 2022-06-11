c884e33892f1c22090d058c983d1c9e032e1e0b8
0xPhoenix10 <jackson.dev.0820@gmail.com>
2022-06-11 19:24:04 +0300

    send email and css fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -96,21 +96,13 @@
                         <div class=" dropdown-header noti-title">
                             <h6 class="text-overflow m-0">Welcome!</h6>
                         </div>
-                        <a href="{{ route('profile.edit') }}" class="dropdown-item">
-                            <i class="ni ni-single-02"></i>
-                            <span>My profile</span>
+                        <!-- <a href="{{ route('profile.edit') }}" class="dropdown-item"> -->
+                        <i class="ni ni-single-02"></i>
+                        <span>My profile</span>
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="account" class="dropdown-item">
                             <i class="ni ni-settings-gear-65"></i>
                             <span>Settings</span>
-                        </a>
-                        <a href="#" class="dropdown-item">
-                            <i class="ni ni-calendar-grid-58"></i>
-                            <span>Activity</span>
-                        </a>
-                        <a href="#" class="dropdown-item">
-                            <i class="ni ni-support-16"></i>
-                            <span>Support</span>
                         </a>
                         <div class="dropdown-divider"></div>
                         <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
@@ -273,21 +265,13 @@
                             <div class=" dropdown-header noti-title">
                                 <h6 class="text-overflow m-0">Welcome!</h6>
                             </div>
-                            <a href="{{ route('profile.edit') }}" class="dropdown-item">
+                            <!-- <a href="{{ route('profile.edit') }}" class="dropdown-item">
                                 <i class="ni ni-single-02"></i>
                                 <span>My profile</span>
-                            </a>
-                            <a href="#" class="dropdown-item">
+                            </a> -->
+                            <a href="account" class="dropdown-item">
                                 <i class="ni ni-settings-gear-65"></i>
                                 <span>Settings</span>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <i class="ni ni-calendar-grid-58"></i>
-                                <span>Activity</span>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <i class="ni ni-support-16"></i>
-                                <span>Support</span>
                             </a>
                             <div class="dropdown-divider"></div>
                             <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
