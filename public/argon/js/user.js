4e72ace752299c658c527125802be74a5ce75296
super-dev0728 <djtonyregent@gmail.com>
2022-06-14 13:05:44 +0300

    delete swal

--- public/argon/js/user.js
+++ public/argon/js/user.js
@@ -3,5 +3,17 @@ function edit_user(id) {
 }
 
 function delete_user(id) {
-    location.href = '/user/delete/' + id;
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "This user will be permanently deleted!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#009683',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            location.href = '/user/delete/' + id;
+        }
+    });
 }
